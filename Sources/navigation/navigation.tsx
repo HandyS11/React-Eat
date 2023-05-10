b09e830390398415b6ed46666f39a0f989acc992
Mathis Ribémont <mathis.ribemont@etu.uca.fr>
2023-05-10 11:24:23 +0200

    code with navigation

---
+++ Sources/navigation/navigation.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { HomeScreen } from '../screens/HomeScreen';
+import { SettingsScreen } from '../screens/SettingsScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function Navigation() {
+  const BottomTabNavigator = createBottomTabNavigator();
+  return (
+      <NavigationContainer>
+          <BottomTabNavigator.Navigator initialRouteName="Home" 
+          screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+              <BottomTabNavigator.Screen name="Home" component={HomeScreen}
+                                         options={{
+                                             title: 'Home',
+                                         }}/>
+              <BottomTabNavigator.Screen name="Settings" component={SettingsScreen}
+                                         options={{
+                                             title: 'Settings',
+                                         }}/>
+          </BottomTabNavigator.Navigator>
+      </NavigationContainer>
+  )
+}
